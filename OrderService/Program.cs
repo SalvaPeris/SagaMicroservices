300b5d3344b264cf3de7f0e8e9013e626363c5bc
Salva Peris <sperisgimeno@gmail.com>
2025-01-24 08:37:48 +0100

    Added publish from OrderService.

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -1,11 +1,28 @@
+using MassTransit;
+using SharedMessages.Messages;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddMassTransit(options =>
+{
+    options.UsingRabbitMq((context, configuration) =>
+    {
+        configuration.Host("rabbitmq://localhost");
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapPost("/orders", async (OrderRequest order, IBus bus) =>
+{
+    var orderPlacedMessage = new OrderPlaced(order.orderId, order.quantity);
+    await bus.Publish(orderPlacedMessage);
+
+    return Results.Created($"/orders/{order.orderId}", orderPlacedMessage);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +33,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+public record OrderRequest(Guid orderId, int quantity);

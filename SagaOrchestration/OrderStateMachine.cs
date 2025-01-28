51dd4a160de71863850f68c558e1a30aa38bfeb1
Salva Peris <sperisgimeno@gmail.com>
2025-01-28 09:43:52 +0100

    Added OrderStateMachine class.

---
+++ SagaOrchestration/OrderStateMachine.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using SharedMessages.Messages;
+
+namespace SagaOrchestration
+{
+    public class OrderStateMachine : MassTransitStateMachine<OrderState>
+    {
+        public State Submitted { get; set; }
+        public State InventoryReserver { get; set; }
+        public State PaymentCompleted { get; set; }
+
+
+        public Event<OrderPlaced> OrderPlacedEvent { get; private set; }
+        public Event<InventoryReserved> InventoryReservedEvent { get; private set; }
+        public Event<PaymentCompleted> PaymentCompletedEvent { get; private set; }
+
+
+        public OrderStateMachine()
+        {
+            InstanceState(x => x.CurrentState);
+
+            Event(() => OrderPlacedEvent, x => x.CorrelateById(context => context.Message.OrderId));
+            Event(() => InventoryReservedEvent, x => x.CorrelateById(context => context.Message.OrderId));
+            Event(() => PaymentCompletedEvent, x => x.CorrelateById(context => context.Message.OrderId));
+
+        }
+    }
+}
